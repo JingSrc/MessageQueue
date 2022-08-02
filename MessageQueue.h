ca301784b13b62e5769877e0030712afbc099aa1
Jing <674923338@qq.com>
2022-08-02 16:30:39 +0800

    Update MessageQueue.h

--- MessageQueue.h
+++ MessageQueue.h
@@ -3,8 +3,8 @@
 #include <memory>
 #include <vector>
 #include <unordered_map>
-#include <typeindex>
 #include <string>
+#include <set>
 
 #include "SharedMutex.h"
 #include "ThreadPool.h"
@@ -41,31 +41,6 @@ private:
 template<typename T>
 using MessagePointer = std::shared_ptr<Message<T>>;
 
-class MessageHandlerIndex final
-{
-    MESSAGE_DISABLE_COPY(MessageHandlerIndex)
-public:
-    using index_type = int;
-
-    static index_type create_index()
-    {
-        auto &inst = instance();
-        return inst.counter_++;
-    }
-
-private:
-    MessageHandlerIndex() = default;
-
-    static MessageHandlerIndex &instance()
-    {
-        static MessageHandlerIndex counter;
-        return counter;
-    }
-
-private:
-    std::atomic_int counter_{0};
-};
-
 template<typename T>
 class IMessageHandler
 {
@@ -75,10 +50,9 @@ public:
     using message_pointer = MessagePointer<value_type>;
 
     explicit IMessageHandler(const std::string &topic, bool once = false)
-            : once_{once}, topic_{topic}, index_{MessageHandlerIndex::create_index()} {}
+            : once_{once}, topic_{topic} {}
     virtual ~IMessageHandler() = default;
 
-    int index() const { return index_; }
     const std::string &topic() const { return topic_; }
     bool once() const { return once_; }
 
@@ -95,11 +69,10 @@ public:
     }
 
 private:
-    bool                            once_;
-    bool                            called_{false};
-    MessageHandlerIndex::index_type index_{-1};
-    std::string                     topic_;
-    std::mutex                      mutex_;
+    bool        once_;
+    bool        called_{false};
+    std::string topic_;
+    std::mutex  mutex_;
 };
 
 template<typename T>
@@ -122,19 +95,10 @@ private:
     message_handler handler_;
 };
 
-class IMessageQueueType
-{
-public:
-    IMessageQueueType() = default;
-    virtual ~IMessageQueueType() = default;
-
-    virtual std::type_index type() const = 0;
-};
-
 template<typename T>
-class MessageQueueType final : public IMessageQueueType
+class MessageQueue final
 {
-    MESSAGE_DISABLE_COPY(MessageQueueType)
+    MESSAGE_DISABLE_COPY(MessageQueue)
 public:
     using value_type              = T;
     using message_type            = Message<value_type>;
@@ -144,14 +108,12 @@ public:
     using message_handler_funtion = std::function<void(const message_pointer &)>;
 
 private:
-    using message_handlers = std::unordered_map<MessageHandlerIndex::index_type, message_handler_pointer>;
+    using message_handlers = std::set<message_handler_pointer>;
 
 public:
-    explicit MessageQueueType(const std::shared_ptr<ThreadPool> &threads)
-            : threads_{threads} {}
-    virtual ~MessageQueueType() override = default;
-
-    virtual std::type_index type() const override { return std::type_index(typeid(value_type)); }
+    explicit MessageQueue()
+            : threads_{new ThreadPool{std::thread::hardware_concurrency()}} {}
+    ~MessageQueue() = default;
 
     bool publish(const std::string &topic, value_type value, bool is_async = true)
     {
@@ -170,8 +132,12 @@ public:
     message_handler_pointer subscribe(message_handler_pointer handler)
     {
         unique_lock<shared_mutex> lock{mutex_};
-        auto &h = handlers_[handler->topic()];
-        h[handler->index()] = handler;
+
+        auto &handlers = handlers_[handler->topic()];
+        if (handlers.find(handler) == handlers.end()) {
+            handlers.insert(handler);
+        }
+
         return handler;
     }
 
@@ -188,7 +154,7 @@ public:
         auto it = handlers_.find(handler->topic());
         if (it != handlers_.end()) {
             auto &handlers = it->second;
-            auto hit = handlers.find(handler->index());
+            auto hit = handlers.find(handler);
             if (hit != handlers.end()) {
                 handlers.erase(hit);
                 if (handlers.empty()) {
@@ -215,9 +181,9 @@ private:
             if (it != handlers_.end()) {
                 auto &handlers = it->second;
                 for (auto hit = handlers.begin(); hit != handlers.end(); ++hit) {
-                    handler(hit->second);
-                    if (hit->second->once()) {
-                        rmv.push_back(hit->second);
+                    handler(*hit);
+                    if ((*hit)->once()) {
+                        rmv.push_back(*hit);
                     }
                 }
             }
@@ -256,135 +222,3 @@ private:
     std::unordered_map<std::string, message_handlers> handlers_;
 };
 
-class MessageQueue
-{
-    MESSAGE_DISABLE_COPY(MessageQueue)
-public:
-    explicit MessageQueue(size_t threads = std::thread::hardware_concurrency())
-            : threads_{new ThreadPool{threads}} {}
-    ~MessageQueue() = default;
-
-    template<typename T>
-    bool publish(const std::string &topic, T value, bool is_async)
-    {
-        auto ptr = std::make_shared<typename MessageQueueType<T>::message_type>(topic, value);
-        return publish<T>(ptr, is_async);
-    }
-
-    template<typename T>
-    bool publish(const typename MessageQueueType<T>::message_pointer &ptr, bool is_async = true)
-    {
-        shared_lock<shared_mutex> lock{ mutex_ };
-        auto qu = get_queue<T>();
-        if (qu) {
-            return qu->publish(ptr, is_async);
-        }
-        return false;
-    }
-
-    template<typename T>
-    std::shared_ptr<IMessageHandler<T>> subscribe(typename MessageQueueType<T>::message_handler_pointer handler)
-    {
-        unique_lock<shared_mutex> lock{mutex_};
-        auto qu = get_queue_default<T>();
-        return qu->subscribe(handler);
-    }
-
-    template<typename T>
-    std::shared_ptr<IMessageHandler<T>> subscribe(const std::string &topic, typename MessageQueueType<T>::message_handler_funtion handler, bool once = false)
-    {
-        unique_lock<shared_mutex> lock{ mutex_ };
-        auto qu = get_queue_default<T>();
-        return qu->subscribe(topic, handler, once);
-    }
-
-    template<typename T>
-    void unsubscribe(int index)
-    {
-        unique_lock<shared_mutex> lock{mutex_};
-        auto qu = get_queue<T>();
-        if (qu) {
-            qu->unsubscribe(index);
-            if (qu->empty()) {
-                queues_.erase(queues_.find(std::type_index(typeid(T))));
-            }
-        }
-    }
-
-private:
-    template<typename T>
-    std::shared_ptr<MessageQueueType<T>> get_queue()
-    {
-        using message_queue_type = MessageQueueType<T>;
-
-        std::shared_ptr<message_queue_type> ptr;
-
-        auto tp = std::type_index(typeid(T));
-        auto it = queues_.find(tp);
-        if (it != queues_.end()) {
-            ptr = std::dynamic_pointer_cast<message_queue_type>(it->second);
-        }
-
-        return ptr;
-    }
-
-    template<typename T>
-    std::shared_ptr<MessageQueueType<T>> get_queue_default()
-    {
-        auto qu = get_queue<T>();
-        if (!qu) {
-            qu = std::make_shared<MessageQueueType<T>>(threads_);
-            queues_[std::type_index(typeid(T))] = qu;
-        }
-
-        return qu;
-    }
-
-private:
-    std::shared_ptr<ThreadPool> threads_;
-    shared_mutex mutex_;
-    std::unordered_map<std::type_index, std::shared_ptr<IMessageQueueType>> queues_;
-};
-
-template<>
-bool MessageQueue::publish<char *>(const typename MessageQueueType<char *>::message_pointer &ptr, bool is_async)
-{
-    shared_lock<shared_mutex> lock{ mutex_ };
-    auto qu = get_queue<char *>();
-    if (qu) {
-        return qu->publish(ptr, is_async);
-    }
-
-    auto cc_qu = get_queue<const char *>();
-    if (cc_qu) {
-        auto cc_ptr = std::make_shared<Message<const char *>>(ptr->topic(), ptr->payload());
-        return cc_qu->publish(cc_ptr, is_async);
-    }
-
-    auto str_qu = get_queue<std::string>();
-    if (str_qu) {
-        auto str_ptr = std::make_shared<Message<std::string>>(ptr->topic(), ptr->payload());
-        return str_qu->publish(str_ptr, is_async);
-    }
-
-    return false;
-}
-
-template<>
-bool MessageQueue::publish<const char *>(const typename MessageQueueType<const char *>::message_pointer &ptr, bool is_async)
-{
-    shared_lock<shared_mutex> lock{ mutex_ };
-
-    auto cc_qu = get_queue<const char *>();
-    if (cc_qu) {
-        return cc_qu->publish(ptr, is_async);
-    }
-
-    auto str_qu = get_queue<std::string>();
-    if (str_qu) {
-        auto str_ptr = std::make_shared<Message<std::string>>(ptr->topic(), ptr->payload());
-        return str_qu->publish(str_ptr, is_async);
-    }
-
-    return false;
-}
